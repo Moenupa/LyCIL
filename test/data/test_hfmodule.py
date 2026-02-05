3efd3ad60124975180eccdd764a8cb898a2365c9
Moenupa <49304833+Moenupa@users.noreply.github.com>
2026-02-05 12:08:09 +0800

    [feat] concat buffer to datamodule

--- test/data/test_hfmodule.py
+++ test/data/test_hfmodule.py
@@ -11,7 +11,7 @@ def cifar10_hfmodule() -> HFDataModule | None:
     if not osp.exists(path):
         return None
 
-    return HFDataModule(path=path)
+    return HFDataModule(path=path, buffer_kwargs={"mem_size": 200})
 
 
 def test_setup_w_seed(cifar10_hfmodule: HFDataModule | None):
@@ -74,3 +74,33 @@ def test_setup_w_custom_labelset(cifar10_hfmodule: HFDataModule | None):
             seen_labels.add(label.item())
     # we have seen task_id \in [0, 3] => 8 classes so far
     assert seen_labels == set(range(8))
+
+
+def test_buffer(cifar10_hfmodule: HFDataModule | None):
+    if cifar10_hfmodule is None:
+        pytest.skip("CIFAR10 dataset not found.")
+        return
+
+    cifar10_hfmodule.label_map = {i: i for i in range(10)}
+    cifar10_hfmodule.num_tasks = 10
+
+    cifar10_hfmodule.prepare_data()
+    cifar10_hfmodule.setup(stage="fit")
+
+    assert cifar10_hfmodule.buffer is not None
+
+    cifar10_hfmodule.buffer["0"] = cifar10_hfmodule.get_filtered_dataset(
+        split=cifar10_hfmodule._split_train,
+        filter_fn=lambda e: e["label"] == 0,
+        transform_name=cifar10_hfmodule.get_effective_transform_name("train"),
+    )
+
+    seen_labels = set()
+    cifar10_hfmodule._cur_task_id = 2
+    train_loader = cifar10_hfmodule.train_dataloader()
+    for batch in train_loader:
+        assert "img" in batch and "label" in batch
+        for label in batch["label"]:
+            seen_labels.add(label.item())
+    # we have seen label==2 training data, and label==0 from buffer replay
+    assert seen_labels == {0, 2}
