dee66c575c0dbcbb592097d104f9ab9347069c9c
Moenupa <49304833+Moenupa@users.noreply.github.com>
2026-02-06 14:49:19 +0800

    [fix] read seed from lightning and add tests for edge cases in data/utils

--- src/lycil/data/util.py
+++ src/lycil/data/util.py
@@ -1,4 +1,6 @@
-from typing import TypedDict
+from typing import Any, TypedDict
+
+from ..constants import get_seed
 
 
 def get_or_identity(mapping: dict[str, str], key: str):
@@ -17,10 +19,10 @@ class SplitMapping(TypedDict, total=False):
     test: str
 
 
-def deterministic_shuffle(lst: list, seed: int | None) -> list:
+def deterministic_shuffle(lst: list) -> list:
     from numpy.random import default_rng
 
-    default_rng(seed).shuffle(lst)
+    default_rng(get_seed()).shuffle(lst)
     return lst
 
 
@@ -34,11 +36,25 @@ def get_num_classes_per_task(
         raise ValueError("`num_classes_per_task` and `num_tasks` cannot be both None.")
 
     if isinstance(num_classes_per_task, list):
+        if num_tasks is not None and len(num_classes_per_task) != num_tasks:
+            raise ValueError(
+                f"Length of num_classes_per_task {len(num_classes_per_task)} != num_tasks {num_tasks}."
+            )
+        if sum(num_classes_per_task) > num_classes:
+            raise ValueError(
+                f"Exceeding total classes: sum({num_classes_per_task}) > {num_classes}."
+            )
+
         return num_classes_per_task
 
     # if num_classes_per_task is given as int,
     # return [num_classes_per_task, ...], if length not given, try max it
     if isinstance(num_classes_per_task, int):
+        if num_tasks is not None and num_classes_per_task * num_tasks > num_classes:
+            raise ValueError(
+                f"Exceeding total classes: {num_classes_per_task} * {num_tasks} > {num_classes}."
+            )
+
         num_tasks = num_tasks or (num_classes // num_classes_per_task)
         return [num_classes_per_task for _ in range(num_tasks)]
 
@@ -50,13 +66,50 @@ def get_num_classes_per_task(
     return [num_classes_per_task for _ in range(num_tasks)]
 
 
-def chunk(lst: list, n_chunks: int) -> list[list]:
-    n_per_chunk = len(lst) // n_chunks
+def check_bijection(
+    mapping: dict[int, Any] | list[Any],
+    values: list[Any],
+    keys: list[int] | None = None,
+) -> None:
+    """Check if mapping is a bijection from keys->values.
 
-    if n_per_chunk == 0:
-        raise ValueError("Number of chunks is greater than the list length.")
-    if n_chunks * n_per_chunk < len(lst):
-        print("WARN: some items will be dropped due to uneven chunking.")
+    Args:
+        mapping (dict[int, Any] | list[Any]): mapping to check
+        values (list[Any]): target values
+        keys (list[int] | None, optional): If None, 0-index indices for values. (default: None)
+    """
+    import warnings
 
-    chunks = [lst[i * n_per_chunk : (i + 1) * n_per_chunk] for i in range(n_chunks)]
-    return chunks
+    if keys is None:
+        keys = list(range(len(values)))
+
+    if len(mapping) != len(values):
+        warnings.warn(
+            f"Mapping length {len(mapping)} != values length {len(values)}. "
+            + f"Mapping provides {mapping} against values {values}."
+        )
+
+    return
+
+
+def reverse_mapping(mapping: dict[int, Any] | list[Any]) -> dict[Any, int]:
+    """Reverse a bijection mapping from keys->values to values->keys.
+
+    Args:
+        mapping (dict[int, Any] | list[Any]): mapping to reverse
+
+    Returns:
+        dict[Any, int]: reversed mapping
+
+    Raises:
+        TypeError: if mapping is not a dict or list
+    """
+    reverse_lookup: dict[Any, int]
+    if isinstance(mapping, (list, tuple)):
+        reverse_lookup = {v: idx for idx, v in enumerate(mapping)}
+    elif isinstance(mapping, dict):
+        reverse_lookup = {v: idx for idx, v in mapping.items()}
+    else:
+        raise TypeError("mapping must be a tuple, list or dict.")
+
+    return reverse_lookup
--- test/data/test_util.py
+++ test/data/test_util.py
@@ -1,20 +1,41 @@
+import lightning as L
 import pytest
 
-from lycil.data.util import chunk
+from lycil.data.util import deterministic_shuffle, get_num_classes_per_task
 
 
 @pytest.mark.parametrize(
-    "lst, n_chunks, expected",
+    "seed,before,after",
     [
-        ([1, 2, 3, 4, 5, 6], 3, [[1, 2], [3, 4], [5, 6]]),
-        ([1, 2, 3, 4, 5], 2, [[1, 2], [3, 4]]),  # last item dropped
-        ([1, 2, 3], 5, None),  # error case
+        (42, list(range(10)), [5, 6, 0, 7, 3, 2, 4, 9, 1, 8]),
+        (0, [0, 1, 2, 3], [2, 0, 1, 3]),
     ],
 )
-def test_chunk(lst, n_chunks, expected):
-    if expected is None:
-        with pytest.raises(ValueError):
-            chunk(lst, n_chunks)
+def test_determistics_shuffle(seed: int, before: list[int], after: list[int]):
+    L.seed_everything(seed)
+    actual = deterministic_shuffle(before)
+    assert actual == after
+
+
+@pytest.mark.parametrize(
+    "num_classes_per_task,num_classes,num_tasks,expected",
+    [
+        (None, 10, None, ValueError),
+        (1, 10, 11, ValueError),
+        (1, 10, 10, [1] * 10),
+        (None, 10, 3, [3] * 3),
+        (4, 10, None, [4] * 2),
+    ],
+)
+def test_get_num_classes_per_task(
+    num_classes_per_task: int | list[int] | None,
+    num_classes: int,
+    num_tasks: int | None,
+    expected: list[int] | type[Exception],
+):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            get_num_classes_per_task(num_classes_per_task, num_classes, num_tasks)
     else:
-        result = chunk(lst, n_chunks)
-        assert result == expected
+        actual = get_num_classes_per_task(num_classes_per_task, num_classes, num_tasks)
+        assert actual == expected
