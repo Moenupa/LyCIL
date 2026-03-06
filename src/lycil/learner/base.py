8813f9a01895ea049216608037fc6a585cc00fd7
Moenupa <49304833+Moenupa@users.noreply.github.com>
2026-03-06 18:01:14 +0800

    [fix] refactor optim and sched; fix some edge cases

--- src/lycil/learner/base.py
+++ src/lycil/learner/base.py
@@ -90,16 +90,30 @@ class BaseLearner(L.LightningModule):
         Args:
             dm (HFDataModule): Data module to sync with.
         """
-        self.task_id = dm.get_current_task()
+        dm_task_id = dm.get_current_task()
+        if self.task_id is not None and dm_task_id == self.task_id:
+            # in sync, no update needed
+            return
+        if self.task_id is not None and self.task_id < 0:
+            # a special bypass rule for multi-stage training per task,
+            # e.g. first stage with no buffer, second stage with buffer.
+            # this will disable head expansion, to do this, you should:
+            # manually set `learner.set_task_id(-2)`
+            # and reset `learner.set_task_id(cur_task_id)`
+            return
+
+        self.task_id = dm_task_id
 
         incoming_expansion = dm.num_seen_classes - (self.num_seen_classes or 0)
         if incoming_expansion <= 0:
+            # task id increased, but no new classes, likely manually set var bugs
             raise RuntimeError(
                 f"Expect an incoming expansion, got {incoming_expansion} new classes. "
                 + f"Data has {dm.num_seen_classes} seen classes, "
                 + f"but Model has {self.num_seen_classes} seen classes. "
                 + "Ensure that `sync_with_datamodule()` is called after datamodule updates."
             )
+        self.expand_head(incoming_expansion)
 
         self.num_old_classes = self.num_seen_classes or 0
         self.num_seen_classes = dm.num_seen_classes
@@ -150,40 +164,49 @@ class BaseLearner(L.LightningModule):
         # with keys 'l1', 'l2', 'l3', 'l4', 'features', 'logits'
         return fmap
 
+    @staticmethod
+    def _get_optimizer(*args, **kwargs):
+        opt_type = kwargs.pop("type", "sgd")
+        match opt_type:
+            case "sgd":
+                return torch.optim.SGD(*args, **kwargs)
+            case "adamw":
+                return torch.optim.AdamW(*args, **kwargs)
+            case _:
+                raise NotImplementedError(f"Unsupported optimizer: `{opt_type}`")
+
+    @staticmethod
+    def _get_scheduler(*args, **kwargs):
+        sched_type = kwargs.pop("type", "linear_warmup_cosine_annealing")
+        match sched_type:
+            case "linear_warmup_cosine_annealing":
+                return LinearWarmupCosineAnnealingLR(*args, **kwargs)
+            case "cosine_annealing":
+                return lr_scheduler.CosineAnnealingLR(*args, **kwargs)
+            case "step_lr":
+                return lr_scheduler.StepLR(*args, **kwargs)
+            case "multi_step_lr":
+                return lr_scheduler.MultiStepLR(*args, **kwargs)
+            case _:
+                raise NotImplementedError(f"Unsupported scheduler: `{sched_type}`")
+
     def configure_optimizers(self):
         params = [p for p in self.parameters() if p.requires_grad]
 
+        # a waterfall lookup for optimizer/scheduler kwargs:
+        # per-task specific > default (-1) > empty dict
         optim_kwargs = (
             self.per_task_optim_args.get(self.task_id)
             or self.per_task_optim_args.get(-1)
             or {}
         )
-        opt_type = optim_kwargs.pop("type", "sgd")
-        match opt_type:
-            case "sgd":
-                optim = torch.optim.SGD(params, **optim_kwargs)
-            case "adamw":
-                optim = torch.optim.AdamW(params, **optim_kwargs)
-            case _:
-                raise NotImplementedError(f"Unsupported optimizer: `{opt_type}`")
-
         sched_kwargs = (
             self.per_task_sched_args.get(self.task_id)
             or self.per_task_sched_args.get(-1)
             or {}
         )
-        sched_type = sched_kwargs.pop("type", "linear_warmup_cosine_annealing")
-        match sched_type:
-            case "linear_warmup_cosine_annealing":
-                sched = LinearWarmupCosineAnnealingLR(optim, **sched_kwargs)
-            case "cosine_annealing":
-                sched = lr_scheduler.CosineAnnealingLR(optim, **sched_kwargs)
-            case "step_lr":
-                sched = lr_scheduler.StepLR(optim, **sched_kwargs)
-            case "multi_step_lr":
-                sched = lr_scheduler.MultiStepLR(optim, **sched_kwargs)
-            case _:
-                raise NotImplementedError(f"Unsupported scheduler: `{sched_type}`")
+        optim = self._get_optimizer(params, **optim_kwargs)
+        sched = self._get_scheduler(optim, **sched_kwargs)
 
         return {
             "optimizer": optim,
@@ -215,7 +238,6 @@ class BaseLearner(L.LightningModule):
         if stage == "fit":
             dm: HFDataModule = self.trainer.datamodule  # ty: ignore[unresolved-attribute]
             self.sync_with_datamodule(dm)
-            self.expand_head(self.num_seen_classes - self.num_old_classes)
 
     def on_fit_end(self):
         self.snapshot_old()
