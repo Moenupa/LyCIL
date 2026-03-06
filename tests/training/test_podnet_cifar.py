367c87e94fcdf9c1e1bc0915f53eeda54ba8a73d
lwz <905328134@qq.com>
2026-03-07 05:34:18 +0800

    podnet debug

--- tests/training/test_podnet_cifar.py
+++ tests/training/test_podnet_cifar.py
@@ -119,7 +119,8 @@ def test_podnet_cifar100(is_dummy_training: bool):
         dm.train_filter_fn = None
         logger1 = OffsetWandbLogger(
             resume="allow",
-            name=f"force_reset_unfixed_b_mask_distill_b_w_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
+            # name=f"force_reset_unfixed_b_mask_distill_b_w_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
+            name=f"force_reset_unfixed_b_distill_b_w_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
             project="lycil",
             log_model=False,
             tags=["podnet", "cifar100"] + ["pretrained" if USE_PRETRAIN_WEIGHTS else "random_init"],
