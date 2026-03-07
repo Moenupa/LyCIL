ab18a305030cbc29755d09d302342cade536ef37
lwz <905328134@qq.com>
2026-03-08 04:33:53 +0800

    debug: add eval mode

--- tests/training/test_podnet_cifar.py
+++ tests/training/test_podnet_cifar.py
@@ -133,7 +133,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
         logger1 = OffsetWandbLogger(
             resume="allow",
             # name=f"force_reset_unfixed_b_mask_distill_b_w_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
-            name=f"sgd_momentum_nopretrain_v2_snapold_160_t_test_herding_select_buffer_newold_mask_wo_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
+            name=f"sgd_momentum_nopretrain_v2_snapold_160_t_test_herding_select_buffer_onlynew_mask_wo_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
             project="lycil",
             log_model=False,
             tags=["podnet", "cifar100"] + ["pretrained" if USE_PRETRAIN_WEIGHTS else "random_init"],
@@ -168,7 +168,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
             # model.backbone.requires_grad_(False)
             # model.classifier.requires_grad_(True)
             dm.use_buffer = True
-            dm.buffer_only_new = False
+            dm.buffer_only_new = True
             dm.train_filter_fn = lambda e: False
 
             logger2 = OffsetWandbLogger(
