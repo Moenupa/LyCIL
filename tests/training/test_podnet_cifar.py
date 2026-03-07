a426728dad2f609d4112a491b048edde2e6913a9
lwz <905328134@qq.com>
2026-03-07 16:28:38 +0800

    debug: add eval mode

--- tests/training/test_podnet_cifar.py
+++ tests/training/test_podnet_cifar.py
@@ -151,7 +151,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
 
             model.backbone.eval()
             model.backbone.requires_grad_(False)
-            model.classifier.requires_grad_(False)
+            model.classifier.requires_grad_(True)
             dm.use_buffer = True
             dm.train_filter_fn = lambda e: False
 
