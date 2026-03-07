ad2fd94c97b0a2351f9a276034cc14af98fc6031
lwz <905328134@qq.com>
2026-03-07 17:21:50 +0800

    podnet debug

--- tests/training/test_podnet_cifar.py
+++ tests/training/test_podnet_cifar.py
@@ -121,7 +121,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
         logger1 = OffsetWandbLogger(
             resume="allow",
             # name=f"force_reset_unfixed_b_mask_distill_b_w_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
-            name=f"force_reset_b_mask_fixednet_eval_distill_b_wo_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
+            name=f"force_reset_b_mask_fixednet_fixfc1_eval_distill_b_wo_warmup_podnet_cifar100_{'pretrained_' if USE_PRETRAIN_WEIGHTS else ''}task{task_idx}",
             project="lycil",
             log_model=False,
             tags=["podnet", "cifar100"] + ["pretrained" if USE_PRETRAIN_WEIGHTS else "random_init"],
@@ -152,7 +152,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
 
             model.backbone.eval()
             model.backbone.requires_grad_(False)
-            model.classifier.requires_grad_(True)
+            # model.classifier.requires_grad_(True)
             dm.use_buffer = True
             dm.train_filter_fn = lambda e: False
 
