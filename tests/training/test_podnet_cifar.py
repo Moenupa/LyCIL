8a6500b7b3c411d894d7be952fddcccdcc4e3975
lwz <905328134@qq.com>
2026-03-10 01:45:32 +0800

    debug

--- tests/training/test_podnet_cifar.py
+++ tests/training/test_podnet_cifar.py
@@ -51,7 +51,7 @@ def test_podnet_cifar100(is_dummy_training: bool):
         LABEL_COL = "fine_label"
         EPOCHS_PER_TASK = 160
         EPOCHS_PER_TASK_MEMORY = 20
-        USE_PRETRAIN_WEIGHTS = False
+        USE_PRETRAIN_WEIGHTS = True
         BUFFER_SIZE_PER_CLASS = 20
     if not osp.exists(DATAPATH):
         pytest.skip("Data path does not exist.")
