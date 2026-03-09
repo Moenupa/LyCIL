c84775ff1518f348543025023b71b388c939a8ac
lwz <905328134@qq.com>
2026-03-10 03:00:40 +0800

    debug: add eval mode

--- tests/training/test_icarl_cifar_v2.py
+++ tests/training/test_icarl_cifar_v2.py
@@ -51,7 +51,7 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
         transform_name=osp.basename(DATAPATH),
         num_classes_per_task=N_CLASS_PER_TASK,
         label_column_name=LABEL_COL,  # 100 classes
-        train_loader_kwargs={"batch_size": 64, "shuffle": True, "num_workers": 10},
+        train_loader_kwargs={"batch_size": 128, "shuffle": True, "num_workers": 10},
         val_loader_kwargs=VAL_LOADER_KWARGS,
         test_loader_kwargs=TEST_LOADER_KWARGS,
         split_map={"train": "train", "val": "test", "test": "test"},
@@ -68,7 +68,7 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
             # for all tasks, use the same optimizer kwargs
             "default": {
                 "type": "sgd",
-                "lr": 0.3,
+                "lr": 0.1,
                 "momentum": 0 if USE_PRETRAIN_WEIGHTS else 0.9,
                 "weight_decay": 5e-4,
             },
@@ -76,10 +76,9 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
         per_task_sched_args={
             # for all tasks, use the same scheduler kwargs
             "default": {
-                "type": "linear_warmup_cosine_annealing",
-                "warmup_epochs": 0 if EPOCHS_PER_TASK == 1 else 10,
-                "max_epochs": EPOCHS_PER_TASK,
-            }
+                "type": "cosine_annealing",
+                "T_max": EPOCHS_PER_TASK,
+            },
         },
         distill_T=1.0,
         distill_lambda=0.1,
