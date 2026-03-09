72629f6259b41f98201f2448d1dae0ba84e36544
lwz <905328134@qq.com>
2026-03-10 02:55:56 +0800

    debug: add eval mode

--- tests/training/test_ewc_cifar_v2.py
+++ tests/training/test_ewc_cifar_v2.py
@@ -98,7 +98,7 @@ def test_ewc_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"ewc_cifar100_task{task_idx}",
+                name=f"hparms_from_pod_ewc_cifar100_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["ewc", "cifar100"],
