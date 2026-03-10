73da68ec5f6513d8a82ca6c3c8051473aa90cbcb
lwz <905328134@qq.com>
2026-03-10 23:06:34 +0800

    debug

--- tests/training/test_ewc_cifar_v2.py
+++ tests/training/test_ewc_cifar_v2.py
@@ -67,7 +67,7 @@ def test_ewc_cifar100(device: str, is_dummy_training: bool):
                 "type": "sgd",
                 "lr": 0.1,
                 "momentum": 0 if USE_PRETRAIN_WEIGHTS else 0.9,
-                "weight_decay": 2e-4,
+                "weight_decay": 5e-4,
             },
             0: {
                 "type": "sgd",
