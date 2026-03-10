557a80983b49cfa4ab9fb536ad2725bc1dc65209
lwz <905328134@qq.com>
2026-03-11 00:57:24 +0800

    debug

--- tests/training/test_podnet_cifar_v2.py
+++ tests/training/test_podnet_cifar_v2.py
@@ -106,10 +106,10 @@ def test_podnet_cifar100(is_dummy_training: bool):
         lambda_spatial=5.0,
         lambda_flat=1.0,
         nme_eval_args={
-            "enable": False,
+            "enable": True,
             "topk": 1,
-            "dynamic_old": True,
-            "dynamic_new": True,
+            "dynamic_old": False,
+            "dynamic_new": False,
             "selection": "random",
             "seed": 42,
             "loader_kwargs": {
