1a8b37d9569d9fbf6da7c2971cdc194b76aa3015
lwz <905328134@qq.com>
2026-03-11 23:17:43 +0800

    debug

--- tests/training/test_icarl_cifar.py
+++ tests/training/test_icarl_cifar.py
@@ -4,12 +4,13 @@ import lightning as L
 import pytest
 from lightning.pytorch.loggers import WandbLogger
 
+
 import wandb
 from lycil.constants import _EXP_NAME
 from lycil.data.hfmodule import HFDataModule
 from lycil.learner.icarl import ICaRL
 
-from .constants import (
+from tests.training.constants import (
     CIFAR10_LABEL_COL,
     CIFAR10_PATH,
     CIFAR100_LABEL_COL,
@@ -18,9 +19,10 @@ from .constants import (
     TEST_LOADER_KWARGS,
     VAL_LOADER_KWARGS,
 )
+from lightning.pytorch.callbacks import LearningRateMonitor
+from lycil.backbone import ConvNetArgs
 
-BUFFER_SIZE_PER_CLASS = 20
-
+from tests.training.log_utils import log_statistics_to_wandb
 
 @pytest.mark.slow
 @pytest.mark.runs_on(["cuda"])
@@ -31,50 +33,76 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
         N_CLASS_PER_TASK = [1, 1]
         EPOCHS_PER_TASK = 1
     else:
-        DATAPATH, LABEL_COL = CIFAR100_PATH, CIFAR100_LABEL_COL
+        DATAPATH = "/ppio_net0/datasets/cifar100"
         N_CLASS_PER_TASK = [20, 20, 20, 20, 20]
-        EPOCHS_PER_TASK = 80
+        LABEL_COL = "fine_label"
+        EPOCHS_PER_TASK = 160
+        USE_PRETRAIN_WEIGHTS = False
+        BUFFER_SIZE_PER_CLASS = 20
     if not osp.exists(DATAPATH):
         pytest.skip("Data path does not exist.")
         return
 
     L.seed_everything(42)
+
+    total_buffer_size = BUFFER_SIZE_PER_CLASS * sum(N_CLASS_PER_TASK)
     dm = HFDataModule(
         DATAPATH,
         transform_name=osp.basename(DATAPATH),
         num_classes_per_task=N_CLASS_PER_TASK,
         label_column_name=LABEL_COL,  # 100 classes
-        train_loader_kwargs={"batch_size": 64, "shuffle": True, "num_workers": 10},
+        train_loader_kwargs={"batch_size": 128, "shuffle": True, "num_workers": 10},
         val_loader_kwargs=VAL_LOADER_KWARGS,
         test_loader_kwargs=TEST_LOADER_KWARGS,
-        split_map={"train": "test", "val": "test"}
-        if is_dummy_training
-        else {"val": "test"},
-        buffer_kwargs={"mem_size_per_class": BUFFER_SIZE_PER_CLASS},
+        split_map={"train": "train", "val": "test", "test": "test"},
+        # buffer_kwargs={"mem_size_per_class": BUFFER_SIZE_PER_CLASS},
+        # Use an adaptive total-memory budget so early tasks can temporarily
+        # occupy the slots of unseen future classes.
+        buffer_kwargs={"mem_size": total_buffer_size},
+        # buffer_kwargs={"mem_size_per_class": BUFFER_SIZE_PER_CLASS},
     )
     model = ICaRL(
-        backbone_args=CONVNET_ARGS[is_dummy_training],
+        backbone_args=ConvNetArgs(name="resnet50", pretrained=USE_PRETRAIN_WEIGHTS, cifar=True),
         head="linear",
         per_task_optim_args={
             # for all tasks, use the same optimizer kwargs
-            -1: {
+            "default": {
                 "type": "sgd",
-                "lr": 0.3,
+                "lr": 0.1,
+                "momentum": 0 if USE_PRETRAIN_WEIGHTS else 0.9,
                 "weight_decay": 5e-4,
             },
         },
         per_task_sched_args={
             # for all tasks, use the same scheduler kwargs
-            -1: {
+            "default": {
                 "type": "linear_warmup_cosine_annealing",
                 "warmup_epochs": 0 if EPOCHS_PER_TASK == 1 else 10,
                 "max_epochs": EPOCHS_PER_TASK,
-            }
+            },
         },
         distill_T=1.0,
         distill_lambda=0.1,
+        buffer_args={
+            "selection": "herding",
+            "seed": 42,
+            "loader_kwargs": {
+                "batch_size": 128,
+                "shuffle": False,
+                "num_workers": 10,
+            },
+            "nme_eval": {
+                "enable": True,
+                "topk": 1,
+                "dynamic_old": True,
+                "dynamic_new": True,
+                "every_n_epochs": EPOCHS_PER_TASK,
+            },
+        },
+
     )
 
+    statistics_summary={}
     for task_idx, _ in enumerate(N_CLASS_PER_TASK):
         dm.set_current_task(task_idx)
 
@@ -83,7 +111,7 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
             max_epochs=EPOCHS_PER_TASK,
             sync_batchnorm=True,
             enable_checkpointing=False,
-            enable_progress_bar=False,
+            enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
                 name=f"icarl_cifar100_task{task_idx}",
@@ -92,11 +120,18 @@ def test_icarl_cifar100(device: str, is_dummy_training: bool):
                 tags=["icarl", "cifar100"],
                 group=_EXP_NAME,
             ),
-            check_val_every_n_epoch=10,
-            log_every_n_steps=1000,
+            check_val_every_n_epoch=1,
+            callbacks=[LearningRateMonitor(logging_interval="epoch")],
         )
         trainer.fit(model, datamodule=dm)
-        trainer.validate(model, datamodule=dm)
+        test_outputs = trainer.test(
+            model=model,
+            datamodule=dm,
+            verbose=False,
+            ckpt_path=None,
+        )
+        statistics_summary[task_idx] = test_outputs
+        log_statistics_to_wandb(trainer, statistics_summary)
 
         wandb.finish()
 
