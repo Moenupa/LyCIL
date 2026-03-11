cc769ad9503f39f2de2b98141718b1df73048466
lwz <905328134@qq.com>
2026-03-11 23:13:00 +0800

    debug

--- tests/training/test_lwf_cifar.py
+++ tests/training/test_lwf_cifar.py
@@ -9,7 +9,7 @@ from lycil.constants import _EXP_NAME
 from lycil.data.hfmodule import HFDataModule
 from lycil.learner.lwf import LWF
 
-from .constants import (
+from tests.training.constants import (
     CIFAR10_LABEL_COL,
     CIFAR10_PATH,
     CIFAR100_LABEL_COL,
@@ -18,7 +18,9 @@ from .constants import (
     TEST_LOADER_KWARGS,
     VAL_LOADER_KWARGS,
 )
-
+from lightning.pytorch.callbacks import LearningRateMonitor
+from lycil.backbone import ConvNetArgs
+from tests.training.log_utils import log_statistics_to_wandb
 
 @pytest.mark.slow
 @pytest.mark.runs_on(["cuda"])
@@ -29,49 +31,54 @@ def test_lwf_cifar100(device: str, is_dummy_training: bool):
         N_CLASS_PER_TASK = [1, 1]
         EPOCHS_PER_TASK = 1
     else:
-        DATAPATH, LABEL_COL = CIFAR100_PATH, CIFAR100_LABEL_COL
+        DATAPATH = "/ppio_net0/datasets/cifar100"
         N_CLASS_PER_TASK = [20, 20, 20, 20, 20]
-        EPOCHS_PER_TASK = 80
+        LABEL_COL = "fine_label"
+        EPOCHS_PER_TASK = 160
+        USE_PRETRAIN_WEIGHTS = False
     if not osp.exists(DATAPATH):
         pytest.skip("Data path does not exist.")
         return
 
     L.seed_everything(42)
+
     dm = HFDataModule(
         DATAPATH,
         transform_name=osp.basename(DATAPATH),
         num_classes_per_task=N_CLASS_PER_TASK,
         label_column_name=LABEL_COL,  # 100 classes
-        train_loader_kwargs={"batch_size": 64, "shuffle": True, "num_workers": 10},
+        train_loader_kwargs={"batch_size": 128, "shuffle": True, "num_workers": 10},
         val_loader_kwargs=VAL_LOADER_KWARGS,
         test_loader_kwargs=TEST_LOADER_KWARGS,
-        split_map={"train": "test", "val": "test"}
-        if is_dummy_training
-        else {"val": "test"},
+        split_map={"train": "train", "val": "test", "test": "test"},
+        buffer_kwargs=None,
     )
     model = LWF(
-        backbone_args=CONVNET_ARGS[is_dummy_training],
+        backbone_args=ConvNetArgs(name="resnet50", pretrained=USE_PRETRAIN_WEIGHTS, cifar=True),
         head="linear",
         per_task_optim_args={
             # for all tasks, use the same optimizer kwargs
-            -1: {
+            "default": {
                 "type": "sgd",
-                "lr": 0.3,
+                "lr": 0.1,
+                "momentum": 0 if USE_PRETRAIN_WEIGHTS else 0.9,
                 "weight_decay": 5e-4,
             },
         },
         per_task_sched_args={
             # for all tasks, use the same scheduler kwargs
-            -1: {
+            "default": {
                 "type": "linear_warmup_cosine_annealing",
                 "warmup_epochs": 0 if EPOCHS_PER_TASK == 1 else 10,
                 "max_epochs": EPOCHS_PER_TASK,
-            }
+            },
         },
         distill_T=1.0,
-        distill_lambda=0.1,
+        distill_lambda=1.0,
+        buffer_args=None,
     )
 
+    statistics_summary={}
     for task_idx, _ in enumerate(N_CLASS_PER_TASK):
         dm.set_current_task(task_idx)
 
@@ -80,20 +87,27 @@ def test_lwf_cifar100(device: str, is_dummy_training: bool):
             max_epochs=EPOCHS_PER_TASK,
             sync_batchnorm=True,
             enable_checkpointing=False,
-            enable_progress_bar=False,
+            enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"lwf_cifar100_task{task_idx}",
+                name=f"lambda1.0_lwf_cifar100_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["lwf", "cifar100"],
                 group=_EXP_NAME,
             ),
-            check_val_every_n_epoch=10,
-            log_every_n_steps=1000,
+            check_val_every_n_epoch=1,
+            callbacks=[LearningRateMonitor(logging_interval="epoch")],
         )
         trainer.fit(model, datamodule=dm)
-        trainer.validate(model, datamodule=dm)
+        test_outputs = trainer.test(
+            model=model,
+            datamodule=dm,
+            verbose=False,
+            ckpt_path=None,
+        )
+        statistics_summary[task_idx] = test_outputs
+        log_statistics_to_wandb(trainer, statistics_summary)
 
         wandb.finish()
 
