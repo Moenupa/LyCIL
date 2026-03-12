becf54ae5ad1f8feec2c08ae58fd14068a50f666
lwz <905328134@qq.com>
2026-03-13 00:47:34 +0800

    debug

--- tests/training/test_der_cifar.py
+++ tests/training/test_der_cifar.py
@@ -109,8 +109,8 @@ def test_der_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"der_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
-                project="lycil_cifar100",
+                name=f"der_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
+                project="lycil",
                 log_model=False,
                 tags=["der", "cifar100"],
                 group=_EXP_NAME,
