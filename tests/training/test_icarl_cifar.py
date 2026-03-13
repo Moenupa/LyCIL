24969e7831d1af03e801989fbbb4ca228cf0acf1
lwz <905328134@qq.com>
2026-03-13 10:16:39 +0800

    debug

--- tests/training/test_icarl_cifar.py
+++ tests/training/test_icarl_cifar.py
@@ -30,7 +30,7 @@ import math
 @pytest.mark.xdist_group("training")
 def test_icarl_cifar100(accelerator: str, is_dummy_training: bool):
     DATAPATH = "/ppio_net0/datasets/cifar100"
-    N_CLASS_PER_TASK = [50] + [10] * 5
+    N_CLASS_PER_TASK = [10] * 10
     LABEL_COL = "fine_label"
     EPOCHS_PER_TASK = 160
     USE_PRETRAIN_WEIGHTS = False
@@ -125,6 +125,7 @@ def test_icarl_cifar100(accelerator: str, is_dummy_training: bool):
             ),
             check_val_every_n_epoch=1,
             callbacks=[LearningRateMonitor(logging_interval="epoch")],
+            gradient_clip_val=0.5
         )
         trainer.fit(model, datamodule=dm)
         test_outputs = trainer.test(
--- tests/training/test_ucir_cifar.py
+++ tests/training/test_ucir_cifar.py
@@ -34,7 +34,7 @@ def test_ucir_cifar100(device: str, is_dummy_training: bool):
         EPOCHS_PER_TASK = 1
     else:
         DATAPATH = "/ppio_net0/datasets/cifar100"
-        N_CLASS_PER_TASK = [50] + [10] * 5
+        N_CLASS_PER_TASK = [10] * 10
         LABEL_COL = "fine_label"
         EPOCHS_PER_TASK = 160
         USE_PRETRAIN_WEIGHTS = False
