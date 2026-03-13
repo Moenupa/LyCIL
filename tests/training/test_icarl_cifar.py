9a17f0e79f6d17858e44ed41a7afbc0476e9c069
lwz <905328134@qq.com>
2026-03-13 10:26:54 +0800

    debug

--- tests/training/test_icarl_cifar.py
+++ tests/training/test_icarl_cifar.py
@@ -125,7 +125,7 @@ def test_icarl_cifar100(accelerator: str, is_dummy_training: bool):
             ),
             check_val_every_n_epoch=1,
             callbacks=[LearningRateMonitor(logging_interval="epoch")],
-            gradient_clip_val=1.0
+            gradient_clip_val=1.5
         )
         trainer.fit(model, datamodule=dm)
         test_outputs = trainer.test(
