0fc18d87b53f3b9bb961fb8a25a4712698041792
lwz <905328134@qq.com>
2026-03-13 10:23:54 +0800

    debug

--- tests/training/test_icarl_cifar.py
+++ tests/training/test_icarl_cifar.py
@@ -138,7 +138,7 @@ def test_icarl_cifar100(accelerator: str, is_dummy_training: bool):
         log_statistics_to_wandb(trainer, statistics_summary)
 
         wandb.finish()
-        # break
+        break
 
 
 if __name__ == "__main__":
