bbddd0d9bc07be25721bf1fd6511ff2993ba7563
lwz <905328134@qq.com>
2026-03-15 03:36:18 +0800

    debug

--- tests/training/test_pass_cifar.py
+++ tests/training/test_pass_cifar.py
@@ -97,7 +97,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
                 tags=["pass", "cifar100"],
                 group=_EXP_NAME,
             ),
-            check_val_every_n_epoch=40,
+            check_val_every_n_epoch=1,
             callbacks=[LearningRateMonitor(logging_interval="epoch")],
             # gradient_clip_val=1.0
         )
