33e7c93a7ec1133608a927ec6fcd35084e921cad
lwz <905328134@qq.com>
2026-03-15 03:11:04 +0800

    debug

--- tests/training/test_il2a_cifar.py
+++ tests/training/test_il2a_cifar.py
@@ -99,7 +99,7 @@ def test_il2a_cifar100(device: str, is_dummy_training: bool):
                 tags=["il2a", "cifar100"],
                 group=_EXP_NAME,
             ),
-            check_val_every_n_epoch=1,
+            check_val_every_n_epoch=40,
             callbacks=[LearningRateMonitor(logging_interval="epoch")],
             gradient_clip_val=1.0
         )
