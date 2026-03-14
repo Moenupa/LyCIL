0f745767cbffe80093d5ddd2c345fc57d1dc0a5a
lwz <905328134@qq.com>
2026-03-15 02:25:49 +0800

    debug

--- tests/training/test_pass_cifar.py
+++ tests/training/test_pass_cifar.py
@@ -123,6 +123,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
         log_statistics_to_wandb(trainer, statistics_summary)
 
         wandb.finish()
+        break
 
 
 if __name__ == "__main__":
