8e35e9afdd7915362d39c0914b372d30032c99f6
lwz <905328134@qq.com>
2026-03-15 16:11:44 +0800

    debug

--- tests/training/test_ssre_cifar.py
+++ tests/training/test_ssre_cifar.py
@@ -48,7 +48,7 @@ def test_ssre_cifar100(device: str, is_dummy_training: bool):
         transform_name=osp.basename(DATAPATH),
         num_classes_per_task=N_CLASS_PER_TASK,
         label_column_name=LABEL_COL,  # 100 classes
-        train_loader_kwargs={"batch_size": 64, "shuffle": True, "num_workers": 8},
+        train_loader_kwargs={"batch_size": 128, "shuffle": True, "num_workers": 8},
         val_loader_kwargs=VAL_LOADER_KWARGS,
         test_loader_kwargs=TEST_LOADER_KWARGS,
         split_map={"train": "train", "val": "test", "test": "test"},
