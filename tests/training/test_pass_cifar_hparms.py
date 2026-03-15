d543128a28a8bae9353122b2cb5c5e8f03f0abf5
lwz <905328134@qq.com>
2026-03-15 08:13:06 +0800

    debug

--- tests/training/test_pass_cifar_hparms.py
+++ tests/training/test_pass_cifar_hparms.py
@@ -75,7 +75,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
             },
         },
         temp=0.1,
-        lambda_fkd=0.1,
+        lambda_fkd=10,
         lambda_proto=10,
         num_rotations=4,
         buffer_args=None,
