6c0c67958eef746ff7b440e8cbc4adaba56d8064
lwz <905328134@qq.com>
2026-03-15 15:21:04 +0800

    debug

--- tests/training/test_ssre_cifar.py
+++ tests/training/test_ssre_cifar.py
@@ -48,7 +48,7 @@ def test_ssre_cifar100(device: str, is_dummy_training: bool):
         transform_name=osp.basename(DATAPATH),
         num_classes_per_task=N_CLASS_PER_TASK,
         label_column_name=LABEL_COL,  # 100 classes
-        train_loader_kwargs={"batch_size": 128, "shuffle": True, "num_workers": 8},
+        train_loader_kwargs={"batch_size": 64, "shuffle": True, "num_workers": 8},
         val_loader_kwargs=VAL_LOADER_KWARGS,
         test_loader_kwargs=TEST_LOADER_KWARGS,
         split_map={"train": "train", "val": "test", "test": "test"},
@@ -61,7 +61,7 @@ def test_ssre_cifar100(device: str, is_dummy_training: bool):
             # for all tasks, use the same optimizer kwargs
             "default": {
                 "type": "adam",
-                "lr": 2e-3,
+                "lr": 1e-3,
                 "weight_decay": 5e-4,
             },
         },
@@ -72,7 +72,7 @@ def test_ssre_cifar100(device: str, is_dummy_training: bool):
                 "T_max": EPOCHS_PER_TASK,
             },
         },
-        temp= 1.0,
+        temp= 0.1,
         lambda_kd = 1.0,
         lambda_proto = 10,
         buffer_args=None,
