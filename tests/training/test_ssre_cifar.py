029fc8d23cbe9bd2d9050a56796d37f5b989a63d
lwz <905328134@qq.com>
2026-03-17 02:33:31 +0800

    debug: add eval mode

--- tests/training/test_ssre_cifar.py
+++ tests/training/test_ssre_cifar.py
@@ -90,7 +90,7 @@ def test_ssre_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"ssre_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
+                name=f"aug_ssre_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["ssre", "cifar100"],
