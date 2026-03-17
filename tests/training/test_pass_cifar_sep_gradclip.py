dfa9581c4e7dbf00cb9cc11fda5e87593d483d0a
lwz <905328134@qq.com>
2026-03-18 03:12:15 +0800

    debug

--- tests/training/test_pass_cifar_sep_gradclip.py
+++ tests/training/test_pass_cifar_sep_gradclip.py
@@ -77,7 +77,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
         },
         temp=0.1,
         lambda_kd=1.0,
-        lambda_proto=1e5,
+        lambda_proto=1e6,
         num_rotations=4,
         buffer_args=None,
     )
@@ -94,7 +94,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"lambda1.0_proto1e5_sgd_gradclip10.0_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
+                name=f"lambda1.0_proto1e6_sgd_gradclip20.0_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["pass", "cifar100"],
@@ -102,7 +102,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
             ),
             check_val_every_n_epoch=1,
             callbacks=[LearningRateMonitor(logging_interval="epoch")],
-            gradient_clip_val=1.0 if task_idx==0 else 10.0,
+            gradient_clip_val=1.0 if task_idx==0 else 20.0,
         )
         trainer.fit(model, datamodule=dm)
         test_outputs = trainer.test(
