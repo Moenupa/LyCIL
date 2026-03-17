dcd4d788321b842a3b9339f6775191bf0948885c
lwz <905328134@qq.com>
2026-03-17 12:59:08 +0800

    debug: add eval mode

--- tests/training/test_pass_cifar_hparams.py
+++ tests/training/test_pass_cifar_hparams.py
@@ -109,7 +109,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"aug_lambda1.0_sgd_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
+                name=f"lambda1.0_sgd_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["pass", "cifar100"],
