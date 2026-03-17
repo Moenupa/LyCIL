31fa2c69e40095d00162c286bb81302c545984fb
lwz <905328134@qq.com>
2026-03-18 00:06:33 +0800

    debug

--- tests/training/test_pass_cifar_sep_gradclip.py
+++ tests/training/test_pass_cifar_sep_gradclip.py
@@ -58,21 +58,6 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
     model = PASS(
         backbone_args=ConvNetArgs(name="resnet50", pretrained=USE_PRETRAIN_WEIGHTS, cifar=True),
         head="linear",
-        # per_task_optim_args={
-        #     # for all tasks, use the same optimizer kwargs
-        #     "default": {
-        #         "type": "adam",
-        #         "lr": 1e-3,
-        #         "weight_decay": 5e-4,
-        #     },
-        # },
-        # per_task_sched_args={
-        #     # for all tasks, use the same scheduler kwargs
-        #     "default": {
-        #         "type": "cosine_annealing",
-        #         "T_max": EPOCHS_PER_TASK,
-        #     },
-        # },
         per_task_optim_args={
             # for all tasks, use the same optimizer kwargs
             "default": {
