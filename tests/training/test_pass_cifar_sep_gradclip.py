6d61b1909256a8ef4c9ece550090b23a529222c4
lwz <905328134@qq.com>
2026-03-18 21:34:25 +0800

    debug

--- tests/training/test_pass_cifar_sep_gradclip.py
+++ tests/training/test_pass_cifar_sep_gradclip.py
@@ -66,7 +66,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
     for task_idx in range(1, len(N_CLASS_PER_TASK)):
         per_task_optim_args[task_idx] = {
             "type": "sgd",
-            "lr": 1e-3,  # task > 0
+            "lr": 5e-3,  # task > 0
             "momentum": 0 if USE_PRETRAIN_WEIGHTS else 0.9,
             "weight_decay": 5e-4,
         }
@@ -111,7 +111,7 @@ def test_pass_cifar100(device: str, is_dummy_training: bool):
             enable_progress_bar=True,
             precision="16-mixed",
             logger=WandbLogger(
-                name=f"lambda0.1_proto1e5_sgd_gradclip1.0_lr1e-3_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
+                name=f"lambda0.1_proto1e5_sgd_gradclip1.0_lr5e-3_pass_cifar100_T{len(N_CLASS_PER_TASK)}_task{task_idx}",
                 project="lycil",
                 log_model=False,
                 tags=["pass", "cifar100"],
